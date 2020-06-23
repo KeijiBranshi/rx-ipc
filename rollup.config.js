89bbd77b3d3f8896b238e670788d887b5dfc773b
Brandon Horton <brhorto@microsoft.com>
2020-06-23 12:26:01 -0700

    extrernalized somet libraries

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
-import { eslint } from 'rollup-plugin-eslint';
-import typescript from 'rollup-plugin-typescript2';
+import { eslint } from "rollup-plugin-eslint";
+import typescript from "rollup-plugin-typescript2";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
-    dir: 'dist',
-    format: 'cjs',
+    dir: "dist",
+    format: "cjs",
   },
-  plugins: [typescript(), eslint({ throwOnError: true })],
+  plugins: [eslint({ throwOnError: true }), typescript()],
+  external: ["rxjs", "node-uuid", "electron"],
 };
