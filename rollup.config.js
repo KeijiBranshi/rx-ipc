833f8a684f4138ae52ec4ecccce6c9aa90ce811b
Brandon Horton <brhorto@microsoft.com>
2020-06-23 01:34:08 -0700

    formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from "rollup-plugin-typescript2";
+import { eslint } from "rollup-plugin-eslint";
+
+const eslintOptions = {
+  fix: true,
+  throwOnError: true,
+};
 
 export default {
-    input: 'src/index.ts',
-    output: {
-        dir: 'dist',
-        format: 'cjs'
-    },
-    plugins: [typescript()]
+  input: "src/index.ts",
+  output: {
+    dir: "dist",
+    format: "cjs",
+  },
+  plugins: [typescript(), eslint(eslintOptions)],
 };
