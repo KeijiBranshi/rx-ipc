34d539e6495334a3f443938264acac07314ff1a1
Brandon Horton <brhorto@microsoft.com>
2020-06-23 01:38:30 -0700

    removed ad hoc config

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,11 @@
 import typescript from "rollup-plugin-typescript2";
 import { eslint } from "rollup-plugin-eslint";
 
-const eslintOptions = {
-  fix: true,
-  throwOnError: true,
-};
-
 export default {
   input: "src/index.ts",
   output: {
     dir: "dist",
     format: "cjs",
   },
-  plugins: [typescript(), eslint(eslintOptions)],
+  plugins: [typescript(), eslint({ throwOnError: true })],
 };
