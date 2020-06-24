a630d568fbf540e42c867192a781c7e9e5da25a4
Brandon <bkhorton@usc.edu>
2020-06-23 19:50:19 -0700

    fixed some lint warnings

--- src/create-proxy-observable.test.ts
+++ src/create-proxy-observable.test.ts
@@ -3,7 +3,7 @@ import { createProxy } from "./create-proxy-observable";
 
 describe("createProxy Tests", () => {
   it("should return an Observable", () => {
-    const proxy = createProxy({ ipc: undefined, channel: "foo" });
+    const proxy = createProxy({ ipc: jest.fn(), channel: "foo" });
     expect(proxy).toBeInstanceOf(Observable);
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -11,7 +11,15 @@ export type ProxyOptions = {
   ipc: PartialIpc;
 };
 
-export function ipcObserverChannels(ipcChannel: string, correlationId: string) {
+type ObserverChannels = {
+  next: string;
+  error: string;
+  complete: string;
+};
+export function ipcObserverChannels(
+  ipcChannel: string,
+  correlationId: string,
+): ObserverChannels {
   return {
     next: `${ipcChannel}-${correlationId}-next`,
     error: `${ipcChannel}-${correlationId}-error`,
@@ -19,18 +27,23 @@ export function ipcObserverChannels(ipcChannel: string, correlationId: string) {
   };
 }
 
-export function ipcObservableChannels(ipcChannel: string) {
+type ObservableChannels = {
+  subscribe: string;
+  unsubscribe: string;
+};
+export function ipcObservableChannels(ipcChannel: string): ObservableChannels {
   return {
     subscribe: `${ipcChannel}-subscribed`,
     unsubscribe: `${ipcChannel}-unsubscribed`,
   };
 }
 
+type TeardownLogic = () => void;
 export function observeOn(
   ipc: Pick<PartialIpc, "on" | "off">,
   channel: string,
   listener: (...args: any[]) => void,
-) {
+): TeardownLogic {
   ipc.on(channel, listener);
   return () => ipc.off(channel, listener);
 }
