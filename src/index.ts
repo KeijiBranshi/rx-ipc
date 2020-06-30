8aa2c3ade80914cd97d5078890409868fe0b2ce7
Brandon Horton <brhorto@microsoft.com>
2020-06-30 12:39:30 -0700

    moved location of add operator

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createProxy } from "./create-proxy-observable";
 import { proxify } from "./proxify-operator";
 
+export * from "./types";
 export default createProxy;
 export { createProxy, proxify };
